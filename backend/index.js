8a4060ea18deece3f6bab5d1e7317521bdb7e176
aestas <hawhawba@gmail.com>
2021-08-03 23:32:46 +0900

    Initialize Firestore Database

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,26 @@
 
   const { request, response } = require('express')
   const express = require('express')
+  let admin = require("firebase-admin") // firebase
 
   /*
   config - express
   */
 
   const app = express()
+
+  /*
+  config - firebase
+  */
+
+  let serviceAccount = require("./serviceAccountKey.json")
+  
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+  
+  let db = admin.firestore()
+
 
   /*
   endpoint
